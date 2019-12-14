2da55f0873ec341f4fbea65e5ffb7205ee84860a
jackwener <894362844@qq.com>
2019-12-15 01:03:03 +0800

    2

---
+++ 2.两数相加.rb
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=2 lang=ruby
+#
+# [2] 两数相加
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
+def add_two_numbers(l1, l2)
+    
+end
+# @lc code=end
+
