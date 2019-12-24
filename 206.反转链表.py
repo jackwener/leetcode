79998b86936fbf995f9363a5d2acdb9d372ada5d
jackwener <894362844@qq.com>
2019-12-24 09:24:46 +0800

    206

--- 206.反转链表.py
+++ 206.反转链表.py
@@ -13,6 +13,14 @@
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        
+        pre = None
+        cur = head
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            
+            pre = cur
+            cur = tmp        
+        return pre
 # @lc code=end
 
