de141019e159a34a590cffddd24dcbbce704aa06
jackwener <894362844@qq.com>
2019-12-28 21:03:49 +0800

    155

---
+++ 155.最小栈.py
@@ -0,0 +1,79 @@
+#
+# @lc app=leetcode.cn id=155 lang=python3
+#
+# [155] 最小栈
+#
+# https://leetcode-cn.com/problems/min-stack/description/
+#
+# algorithms
+# Easy (50.61%)
+# Likes:    347
+# Dislikes: 0
+# Total Accepted:    62.1K
+# Total Submissions: 122.4K
+# Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
+#
+# 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+# 
+# 
+# push(x) -- 将元素 x 推入栈中。
+# pop() -- 删除栈顶的元素。
+# top() -- 获取栈顶元素。
+# getMin() -- 检索栈中的最小元素。
+# 
+# 
+# 示例:
+# 
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin();   --> 返回 -3.
+# minStack.pop();
+# minStack.top();      --> 返回 0.
+# minStack.getMin();   --> 返回 -2.
+# 
+# 
+#
+
+# @lc code=start
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        data = []
+        helper = []
+
+    def push(self, x: int) -> None:
+        self.data.append(x)
+        # 关键 1 和关键 2
+        if len(self.helper) == 0 or x <= self.helper[-1]:
+            self.helper.append(x)
+
+    def pop(self):
+        # 关键 3：【注意】不论怎么样，数据栈都要 pop 出元素
+        top = self.data.pop()
+
+        if self.helper and top == self.helper[-1]:
+            self.helper.pop()
+        return top
+
+    def top(self):
+        if self.data:
+            return self.data[-1]
+
+    def getMin(self):
+        if self.helper:
+            return self.helper[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
+
