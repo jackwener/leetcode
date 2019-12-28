6e55b4ea7d1b9bc6fb41f16d15b7033fd4b0d2a1
jackwener <894362844@qq.com>
2019-12-28 22:00:16 +0800

    58

---
+++ 22.括号生成.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+# https://leetcode-cn.com/problems/generate-parentheses/description/
+#
+# algorithms
+# Medium (72.53%)
+# Likes:    647
+# Dislikes: 0
+# Total Accepted:    59.1K
+# Total Submissions: 81.4K
+# Testcase Example:  '3'
+#
+# 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+# 
+# 例如，给出 n = 3，生成结果为：
+# 
+# [
+# ⁠ "((()))",
+# ⁠ "(()())",
+# ⁠ "(())()",
+# ⁠ "()(())",
+# ⁠ "()()()"
+# ]
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+# @lc code=end
+
---
+++ 58.最后一个单词的长度.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode.cn id=58 lang=python3
+#
+# [58] 最后一个单词的长度
+#
+# https://leetcode-cn.com/problems/length-of-last-word/description/
+#
+# algorithms
+# Easy (31.72%)
+# Likes:    158
+# Dislikes: 0
+# Total Accepted:    58.6K
+# Total Submissions: 183.8K
+# Testcase Example:  '"Hello World"'
+#
+# 给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+# 
+# 如果不存在最后一个单词，请返回 0 。
+# 
+# 说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+# 
+# 示例:
+# 
+# 输入: "Hello World"
+# 输出: 5
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = len(s) - 1
+        result = 0
+        while end >= 0 and s[end] == " ":
+            end -= 1
+        if end == -1: 
+            return 0
+
+        start = end
+        while start != 0 and s[start] == " ":
+            start -= 1
+        return end - start
+
+
+# @lc code=end
+
