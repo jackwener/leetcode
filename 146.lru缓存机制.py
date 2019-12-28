0eec0c0dacbb5ecb71ffb17995a0c59e173a0054
jackwener <894362844@qq.com>
2019-12-28 20:52:28 +0800

    146

--- 146.lru缓存机制.py
+++ 146.lru缓存机制.py
@@ -41,76 +41,57 @@
 #
 
 # @lc code=start
-class ListNode:
-    def __init__(self, key=None, value=None):
+class Node:
+    def __init__(self, key, val):
         self.key = key
-        self.value = value
+        self.val = val
         self.prev = None
         self.next = None
 
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.hashmap = {}
-        # 新建两个节点 head 和 tail
-        self.head = ListNode()
-        self.tail = ListNode()
-        # 初始化链表为 head <-> tail
+        # 构建首尾节点, 使之相连
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    # 因为get与put操作都可能需要将双向链表中的某个节点移到末尾，所以定义一个方法
-    def move_node_to_tail(self, key):
-            # 先将哈希表key指向的节点拎出来，为了简洁起名node
-            #      hashmap[key]                               hashmap[key]
-            #           |                                          |
-            #           V              -->                         V
-            # prev <-> node <-> next         pre <-> next   ...   node
-            node = self.hashmap[key]
-            node.prev.next = node.next
-            node.next.prev = node.prev
-            # 之后将node插入到尾节点前
-            #                 hashmap[key]                 hashmap[key]
-            #                      |                            |
-            #                      V        -->                 V
-            # prev <-> tail  ...  node                prev <-> node <-> tail
-            node.prev = self.tail.prev
-            node.next = self.tail
-            self.tail.prev.next = node
-            self.tail.prev = node
+        self.lookup = dict()
+        self.max_len = capacity
 
     def get(self, key: int) -> int:
-        if key in self.hashmap:
-            # 如果已经在链表中了久把它移到末尾（变成最新访问的）
-            self.move_node_to_tail(key)
-        res = self.hashmap.get(key, -1)
-        if res == -1:
-            return res
+        if key in self.lookup:
+            node = self.lookup[key]
+            self.remove(node)
+            self.add(node)
+            return node.val
         else:
-            return res.value
+            return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.hashmap:
-            # 如果key本身已经在哈希表中了就不需要在链表中加入新的节点
-            # 但是需要更新字典该值对应节点的value
-            self.hashmap[key].value = value
-            # 之后将该节点移到末尾
-            self.move_node_to_tail(key)
-        else:
-            if len(self.hashmap) == self.capacity:
-                # 去掉哈希表对应项
-                self.hashmap.pop(self.head.next.key)
-                # 去掉最久没有被访问过的节点，即头节点之后的节点
-                self.head.next = self.head.next.next
-                self.head.next.prev = self.head
-            # 如果不在的话就插入到尾节点前
-            new = ListNode(key, value)
-            self.hashmap[key] = new
-            new.prev = self.tail.prev
-            new.next = self.tail
-            self.tail.prev.next = new
-            self.tail.prev = new
+        if key in self.lookup:
+            self.remove(self.lookup[key])
+        if len(self.lookup) == self.max_len:
+            # 把表头位置节点删除(说明最近的数据值)
+            self.remove(self.head.next)
+        self.add(Node(key, value))
+    # 删除链表节点
+    def remove(self, node):
+        del self.lookup[node.key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    # 加在链表尾
+    def add(self, node):
+        self.lookup[node.key] = node
+        pre_tail = self.tail.prev
+        node.next = self.tail
+        self.tail.prev = node
+        pre_tail.next = node
+        node.prev = pre_tail
+
+
 
 # @lc code=end
 
