ced2965003f42c4f889e21bce34bf5f4b9ad2ae6
jackwener <894362844@qq.com>
2019-12-29 18:06:05 +0800

    326 init

---
+++ 264.丑数-ii.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode.cn id=264 lang=python3
+#
+# [264] 丑数 II
+#
+# https://leetcode-cn.com/problems/ugly-number-ii/description/
+#
+# algorithms
+# Medium (48.30%)
+# Likes:    187
+# Dislikes: 0
+# Total Accepted:    13.7K
+# Total Submissions: 28.3K
+# Testcase Example:  '10'
+#
+# 编写一个程序，找出第 n 个丑数。
+# 
+# 丑数就是只包含质因数 2, 3, 5 的正整数。
+# 
+# 示例:
+# 
+# 输入: n = 10
+# 输出: 12
+# 解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
+# 
+# 说明:  
+# 
+# 
+# 1 是丑数。
+# n 不超过1690。
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        
+# @lc code=end
+
