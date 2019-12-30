b1938717540d551a4637205589cbbc7010b526c2
jackwener <894362844@qq.com>
2019-12-30 09:27:40 +0800

    221

---
+++ 372.超级次方.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=372 lang=python3
+#
+# [372] 超级次方
+#
+# https://leetcode-cn.com/problems/super-pow/description/
+#
+# algorithms
+# Medium (37.07%)
+# Likes:    45
+# Dislikes: 0
+# Total Accepted:    2.4K
+# Total Submissions: 6.4K
+# Testcase Example:  '2\n[3]'
+#
+# 你的任务是计算 a^b 对 1337 取模，a 是一个正整数，b 是一个非常大的正整数且会以数组形式给出。
+# 
+# 示例 1:
+# 
+# 输入: a = 2, b = [3]
+# 输出: 8
+# 
+# 
+# 示例 2:
+# 
+# 输入: a = 2, b = [1,0]
+# 输出: 1024
+# 
+#
+
+# @lc code=start
+class Solution:
+    def superPow(self, a: int, b: List[int]) -> int:
+        
+# @lc code=end
+
