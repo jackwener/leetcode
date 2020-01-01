eb66d0ddf0112e34d4f54f7f70794b175099a865
jackwener <894362844@qq.com>
2020-01-01 22:00:27 +0800

    66

---
+++ 66.加一.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=66 lang=python3
+#
+# [66] 加一
+#
+# https://leetcode-cn.com/problems/plus-one/description/
+#
+# algorithms
+# Easy (42.07%)
+# Likes:    395
+# Dislikes: 0
+# Total Accepted:    104.6K
+# Total Submissions: 247.5K
+# Testcase Example:  '[1,2,3]'
+#
+# 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+# 
+# 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+# 
+# 你可以假设除了整数 0 之外，这个整数不会以零开头。
+# 
+# 示例 1:
+# 
+# 输入: [1,2,3]
+# 输出: [1,2,4]
+# 解释: 输入数组表示数字 123。
+# 
+# 
+# 示例 2:
+# 
+# 输入: [4,3,2,1]
+# 输出: [4,3,2,2]
+# 解释: 输入数组表示数字 4321。
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+# @lc code=end
+
