78c2b7b99c30bbb6e1850fd57f126c3d282713e1
jackwener <894362844@qq.com>
2020-01-04 20:27:48 +0800

    finish

---
+++ 448.找到所有数组中消失的数字.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=448 lang=java
+ *
+ * [448] 找到所有数组中消失的数字
+ *
+ * https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/description/
+ *
+ * algorithms
+ * Easy (54.50%)
+ * Likes:    253
+ * Dislikes: 0
+ * Total Accepted:    21.9K
+ * Total Submissions: 39.9K
+ * Testcase Example:  '[4,3,2,7,8,2,3,1]'
+ *
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ * 
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ * 
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ * 
+ * 示例:
+ * 
+ * 
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ * 
+ * 输出:
+ * [5,6]
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            int newIndex = Math.abs(nums[i]) - 1;
+
+            if (nums[newIndex] > 0) {
+                nums[newIndex] *= -1;
+            }
+        }
+        
+        // Response array that would contain the missing numbers
+        List<Integer> result = new ArrayList<Integer>();
+        
+        for (int i = 1; i <= nums.length; i++) {
+            
+            if (nums[i - 1] > 0) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
+
+// @lc code=end
+
