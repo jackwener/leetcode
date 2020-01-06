22644c23ed2d478676bcd4bd0f3f69e0f36cd902
jackwener <894362844@qq.com>
2020-01-07 01:59:00 +0800

    finish

---
+++ 507.完美数.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=507 lang=java
+ *
+ * [507] 完美数
+ *
+ * https://leetcode-cn.com/problems/perfect-number/description/
+ *
+ * algorithms
+ * Easy (36.72%)
+ * Likes:    43
+ * Dislikes: 0
+ * Total Accepted:    9.6K
+ * Total Submissions: 26.1K
+ * Testcase Example:  '28'
+ *
+ * 对于一个 正整数，如果它和除了它自身以外的所有正因子之和相等，我们称它为“完美数”。
+ * 
+ * 给定一个 整数 n， 如果他是完美数，返回 True，否则返回 False
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入: 28
+ * 输出: True
+ * 解释: 28 = 1 + 2 + 4 + 7 + 14
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 输入的数字 n 不会超过 100,000,000. (1e8)
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        
+    }
+}
+// @lc code=end
+
