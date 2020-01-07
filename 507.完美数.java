ebafb1f5a020f7f29d4d0fce33c48b4946cdddc4
jackwener <894362844@qq.com>
2020-01-07 16:23:48 +0800

    finish

--- 507.完美数.java
+++ 507.完美数.java
@@ -37,7 +37,17 @@
 // @lc code=start
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        
+        if (num <= 1) {
+            return false;
+        }
+        int sum = 1;
+        for(int i = 2;i * i < num; i++){
+            if(num%i == 0){
+                sum += i;
+                sum += num / i;
+            }
+        }
+        return sum == num;
     }
 }
 // @lc code=end
