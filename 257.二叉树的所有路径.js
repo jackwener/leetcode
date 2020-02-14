6156034a3968aa0fa831498097ae4f526c536f34
jackwener <894362844@qq.com>
2020-02-14 15:17:05 +0800

    finish

--- 257.二叉树的所有路径.js
+++ 257.二叉树的所有路径.js
@@ -17,7 +17,13 @@
  * @return {string[]}
  */
 var binaryTreePaths = function(root) {
-    
-};
+    if (root === null) return []
+    if (root.left === null && root.right === null) return [root.val.toString()]
+    let left = binaryTreePaths(root.left)
+    let right = binaryTreePaths(root.right)
+    let arr = left.concat(right)
+    return arr.map(item => root.val.toString() + '->' + item)
+}
+
 // @lc code=end
 
