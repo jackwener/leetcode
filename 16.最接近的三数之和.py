885a1e283ac418138be590b996a696753c1e4474
jackwener <894362844@qq.com>
2020-02-14 15:40:17 +0800

    finish

--- 16.最接近的三数之和.py
+++ 16.最接近的三数之和.py
@@ -7,6 +7,25 @@
 # @lc code=start
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        
+        nums.sort()
+        n = len(nums)
+        if n < 3: return None
+        res = nums[0] + nums[1] + nums[2]       
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left = i + 1
+            right = n - 1
+            while left < right :
+                cur = nums[i] + nums[left] + nums[right]
+                if cur == target:return target
+                if abs(res-target) > abs(cur-target):
+                    res = cur
+                if cur > target:
+                    right -= 1
+                elif cur < target:
+                    left += 1
+        return res
+
 # @lc code=end
 
--- 86.分隔链表.py
+++ 86.分隔链表.py
@@ -11,8 +11,26 @@
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    def partition(self, head: ListNode, x: int) -> ListNode:
-        
+class Solution(object):
+    def partition(self, head, x):
+        before = before_head = ListNode(0)
+        after = after_head = ListNode(0)
+
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+
+            head = head.next
+
+        after.next = None
+        before.next = after_head.next
+
+        return before_head.next
+
+
 # @lc code=end
 
