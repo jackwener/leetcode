c540839b472dfd134c5ec59d9471bd970a8d2247
jackwener <894362844@qq.com>
2020-02-19 14:11:13 +0800

    finish

--- 684.冗余连接.java
+++ 684.冗余连接.java
@@ -6,9 +6,33 @@
 
 // @lc code=start
 class Solution {
+    private int[] res=new int[2];
     public int[] findRedundantConnection(int[][] edges) {
-        
+        int[] father = new int[edges.length+1];
+
+        for(int i = 1;i < father.length; i++)father[i]=i;
+
+        for(int i = 0;i < edges.length; i++){
+            union(edges[i][0], edges[i][1], father);//两个节点之间结合
+        }
+        return res;
+    }
+    
+    public void union(int x, int y, int[] father){
+        int fx = find(x, father);
+        int fy = find(y, father);
+        if(fx == fy){
+            res[0] = x;
+            res[1] = y;
+        }
+        father[fx] = fy;
+    }
+
+    public int find(int x,int[] father){
+        if(x != father[x]) return find(father[x], father);
+        return father[x];
     }
 }
+
 // @lc code=end
 
--- 707.设计链表.java
+++ 707.设计链表.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /*
  * @lc app=leetcode.cn id=707 lang=java
  *
@@ -53,47 +55,108 @@
  */
 
 // @lc code=start
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode prev;
+    ListNode(int val) { this.val = val; }
+  }
+  
 class MyLinkedList {
-
-    /** Initialize your data structure here. */
-    public MyLinkedList() {
-        
-    }
-    
-    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
-    public int get(int index) {
-        
-    }
-    
-    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
-    public void addAtHead(int val) {
-        
-    }
-    
-    /** Append a node of value val to the last element of the linked list. */
-    public void addAtTail(int val) {
-        
-    }
-    
-    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
-    public void addAtIndex(int index, int val) {
-        
-    }
-    
-    /** Delete the index-th node in the linked list, if the index is valid. */
-    public void deleteAtIndex(int index) {
-        
-    }
+int size;
+// sentinel nodes as pseudo-head and pseudo-tail
+ListNode head, tail;
+public MyLinkedList() {
+    size = 0;
+    head = new ListNode(0);
+    tail = new ListNode(0);
+    head.next = tail;
+    tail.prev = head;
 }
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
+public int get(int index) {
+    if (index < 0 || index >= size) return -1;
+
+    ListNode curr;
+    if (index + 1 < size - index){
+      curr = head;
+      for(int i = 0; i < index + 1; ++i) curr = curr.next;
+    } else {
+      curr = tail;
+      for(int i = 0; i < size - index; ++i) curr = curr.prev;
+    }
+    return curr.val;
+}
+
+public void addAtHead(int val) {
+    ListNode pred = head, succ = head.next;
+
+    ++size;
+    ListNode toAdd = new ListNode(val);
+    toAdd.prev = pred;
+    toAdd.next = succ;
+    pred.next = toAdd;
+    succ.prev = toAdd;
+}
+
+public void addAtTail(int val) {
+    ListNode succ = tail, pred = tail.prev;
+
+    ++size;
+    ListNode toAdd = new ListNode(val);
+    toAdd.prev = pred;
+    toAdd.next = succ;
+    pred.next = toAdd;
+    succ.prev = toAdd;
+}
+
+public void addAtIndex(int index, int val) {
+    if (index > size) return;
+
+    if (index < 0) index = 0;
+
+    ListNode pred, succ;
+    if (index < size - index) {
+    pred = head;
+    for(int i = 0; i < index; ++i) pred = pred.next;
+    succ = pred.next;
+    }
+    else {
+    succ = tail;
+    for (int i = 0; i < size - index; ++i) succ = succ.prev;
+    pred = succ.prev;
+    }
+
+    ++size;
+    ListNode toAdd = new ListNode(val);
+    toAdd.prev = pred;
+    toAdd.next = succ;
+    pred.next = toAdd;
+    succ.prev = toAdd;
+}
+
+public void deleteAtIndex(int index) {
+    if (index < 0 || index >= size) return;
+
+    ListNode pred, succ;
+    if (index < size - index) {
+    pred = head;
+    for(int i = 0; i < index; ++i) pred = pred.next;
+    succ = pred.next.next;
+    }
+    else {
+    succ = tail;
+    for (int i = 0; i < size - index - 1; ++i) succ = succ.prev;
+    pred = succ.prev.prev;
+    }
+
+    --size;
+    pred.next = succ;
+    succ.prev = pred;
+}
+}
+  
+
+
 // @lc code=end
 
