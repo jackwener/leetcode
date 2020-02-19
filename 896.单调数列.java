de7dfc62b2b395b32fec88247e6fcffc93c3e329
jackwener <894362844@qq.com>
2020-02-19 16:05:51 +0800

    finish

---
+++ 896.单调数列.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=896 lang=java
+ *
+ * [896] 单调数列
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isMonotonic(int[] A) {
+        return increasing(A) || decreasing(A);
+    }
+
+    public boolean increasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] > A[i+1]) return false;
+        return true;
+    }
+
+    public boolean decreasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] < A[i+1]) return false;
+        return true;
+    }
+}
+
+
+// @lc code=end
+
