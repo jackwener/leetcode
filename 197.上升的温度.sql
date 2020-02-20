7d39ab937d0f85f6d76370934e5c53dd9240c0ff
jackwener <894362844@qq.com>
2020-02-20 16:55:03 +0800

    finish

--- 197.上升的温度.sql
+++ 197.上升的温度.sql
@@ -6,6 +6,10 @@
 
 -- @lc code=start
 # Write your MySQL query statement below
+SELECT b.Id
+FROM Weather as a,Weather as b
+WHERE a.Temperature < b.Temperature and DATEDIFF(a.RecordDate,b.RecordDate) = -1;
+
 
 -- @lc code=end
 
