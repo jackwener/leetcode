a6a8378627b656d5025a0cf1e0ce6ff6eeb5782c
jackwener <894362844@qq.com>
2020-02-21 20:33:02 +0800

    finish

--- 179.最大数.java
+++ 179.最大数.java
@@ -7,34 +7,45 @@ import java.util.Comparator;
  */
 
 // @lc code=start
+// class Solution {
+    // private class NumberComparator implements Comparator<String>{
+    //     @Override
+    //     public int compare(String a, String b){
+    //         return (b+a).compareTo(a+b);
+    //     }
+    // }
+
+    // public String largestNumber(int[] nums) {
+    //     String[] asStrs = new String[nums.length];
+    //     for (int i = 0; i < nums.length; i++) {
+    //         asStrs[i] = String.valueOf(nums[i]);
+    //     }
+
+    //     Arrays.sort(asStrs, new NumberComparator());
+
+    //     if (asStrs[0].equals("0")) {
+    //         return "0";
+    //     }
+
+    //     String largestNumberStr = new String();
+    //     for (String numAsStr : asStrs) {
+    //         largestNumberStr += numAsStr;
+    //     }
+
+    //     return largestNumberStr;
+    // }
+    
+    
+// }
 class Solution {
-    private class NumberComparator implements Comparator<String>{
-        @Override
-        public int compare(String a, String b){
-            return (b+a).compareTo(a+b);
-        }
-    }
+
+    private StringBuilder res;
 
     public String largestNumber(int[] nums) {
-        String[] asStrs = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            asStrs[i] = String.valueOf(nums[i]);
-        }
-
-        Arrays.sort(asStrs, new NumberComparator());
-
-        if (asStrs[0].equals("0")) {
-            return "0";
-        }
-
-        String largestNumberStr = new String();
-        for (String numAsStr : asStrs) {
-            largestNumberStr += numAsStr;
-        }
-
-        return largestNumberStr;
+        res = new StringBuilder();
+        Arrays.stream(nums).boxed().map(x -> x.toString()).sorted((x, y) -> (y + x).compareTo(x + y)).forEach(x -> res.append(x));
+        return res.charAt(0) == '0' ? "0" : res.toString();
     }
-
 }
 // @lc code=end
 
