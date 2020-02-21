8a79e0057a807395b965d53684ab2da863e7126d
jackwener <894362844@qq.com>
2020-02-21 20:31:54 +0800

    finish

--- 179.最大数.java
+++ 179.最大数.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 /*
  * @lc app=leetcode.cn id=179 lang=java
  *
@@ -6,9 +8,33 @@
 
 // @lc code=start
 class Solution {
-    public String largestNumber(int[] nums) {
-        
+    private class NumberComparator implements Comparator<String>{
+        @Override
+        public int compare(String a, String b){
+            return (b+a).compareTo(a+b);
+        }
     }
+
+    public String largestNumber(int[] nums) {
+        String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(asStrs, new NumberComparator());
+
+        if (asStrs[0].equals("0")) {
+            return "0";
+        }
+
+        String largestNumberStr = new String();
+        for (String numAsStr : asStrs) {
+            largestNumberStr += numAsStr;
+        }
+
+        return largestNumberStr;
+    }
+
 }
 // @lc code=end
 
---
+++ 5.最长回文子串.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=java
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        String res = "";
+        boolean[][] dp = new boolean[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1]); //j - i 代表长度减去 1        
+                if (dp[i][j] &&  j - i + 1 > res.length()) {
+                    res = s.substring(i, j + 1);
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
