bdba561f78c9cfbb7904fdc576dbaf5c8dd1a7de
jackwener <894362844@qq.com>
2020-02-25 10:00:20 +0800

    finish

---
+++ 307.区域和检索-数组可修改.java
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=307 lang=java
+ *
+ * [307] 区域和检索 - 数组可修改
+ */
+
+// @lc code=start
+class NumArray {
+    int[] tree;
+    int n;
+    public NumArray(int[] nums) {
+        if (nums.length > 0) {
+            n = nums.length;
+            tree = new int[n * 2];
+            buildTree(nums);
+        }
+    }
+
+    private void buildTree(int[] nums) {
+        for (int i = n, j = 0;  i < 2 * n; i++,  j++)
+            tree[i] = nums[j];
+        for (int i = n - 1; i > 0; --i)
+            tree[i] = tree[i * 2] + tree[i * 2 + 1];
+    }
+
+    void update(int pos, int val) {
+        pos += n;
+        tree[pos] = val;
+        while (pos > 0) {
+            int left = pos;
+            int right = pos;
+            if (pos % 2 == 0) {
+                right = pos + 1;
+            } else {
+                left = pos - 1;
+            }
+            // parent is updated after child is updated
+            tree[pos / 2] = tree[left] + tree[right];
+            pos /= 2;
+        }
+    }
+    
+
+    public int sumRange(int left, int right) {
+        // get leaf with value 'left'
+        left += n;
+        // get leaf with value 'r'
+        right += n;
+        int sum = 0;
+        while (left <= right) {
+            if ((left % 2) == 1) {
+               sum += tree[left];
+               left++;
+            }
+            if ((right % 2) == 0) {
+               sum += tree[right];
+               right--;
+            }
+            left /= 2;
+            right /= 2;
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
+// @lc code=end
+
