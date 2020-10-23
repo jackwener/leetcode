1431354465eca659e79ef32582e81e28ea141047
jackwener <894362844@qq.com>
2020-10-23 20:58:44 +0800

    finish 641 Java

---
+++ 530.二叉搜索树的最小绝对差.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=530 lang=python3
+#
+# [530] 二叉搜索树的最小绝对差
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getMinimumDifference(self, root: TreeNode) -> int:
+# @lc code=end
+
