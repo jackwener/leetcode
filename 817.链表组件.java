b6fa259ceaa7659443f0c5de5fc721d63275b789
jackwener <894362844@qq.com>
2020-11-07 16:07:22 +0800

    817

---
+++ 817.链表组件.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=817 lang=java
+ *
+ * [817] 链表组件
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : G) {
+            set.add(num);
+        }
+        int res = 0;
+        ListNode cur = head;
+        while(cur != null) {
+            if(set.contains(cur.val)) {
+                while(set.remove(cur.val)) {
+                    if(cur.next == null) break;
+                    cur = cur.next;
+                }
+                res++;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
