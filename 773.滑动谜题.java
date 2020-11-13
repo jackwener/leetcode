6c93df6576ee702fb2d082c2b169a53994664a3b
jackwener <894362844@qq.com>
2020-11-13 23:00:10 +0800

    draft

---
+++ 773.滑动谜题.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=773 lang=java
+ *
+ * [773] 滑动谜题
+ */
+
+// @lc code=start
+class Solution {
+    public int slidingPuzzle(int[][] board) {
+
+    }
+}
+// @lc code=end
+
