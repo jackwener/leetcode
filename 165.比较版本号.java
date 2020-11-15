fa26973c1f570dcf0f566eec11b8d88974be2b29
jackwener <894362844@qq.com>
2020-11-15 22:22:09 +0800

    165

--- 165.比较版本号.java
+++ 165.比较版本号.java
@@ -8,18 +8,21 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
         int num1 = 0, num2 = 0;
-        int i = 0, j= 0;
-        while(i < version1.length() && j < version2.length()) {
-            while(i < version1.length() && version1.charAt(i) != '.' ){
-                num1 = num1 * 10 + (int)version1.charAt(i);
-                i ++;
+        int i = 0, j = 0;
+        while (i < version1.length() || j < version2.length()) {
+            while (i < version1.length() && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (int) version1.charAt(i) - 48;
+                i++;
             }
-            while(j < version2.length() && version2.charAt(j) != '.' ) {
-                num2 = num2 * 10 + (int)version2.charAt(j);
-                j ++;
+            while (j < version2.length() && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + (int) version2.charAt(j) - 48;
+                j++;
             }
-            if(num1 != num2) {
-                return num1 - num2;
+
+            System.out.printf("%d %d", num1, num2);
+
+            if (num1 != num2) {
+                return num1 > num2 ? 1 : -1;
             } else {
                 num1 = 0;
                 num2 = 0;
@@ -31,4 +34,3 @@ class Solution {
     }
 }
 // @lc code=end
-
